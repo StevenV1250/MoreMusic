d06c35cc94b18dff469e3f6843648c660a218990
Steven Vega <stevenvega@Stevens-MacBook-Pro-5.local>
2024-05-09 11:56:14 -0700

    debug false

--- app.py
+++ app.py
@@ -140,4 +140,4 @@ def callback():
             return redirect('/all-time-top-tracks')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False)
